6d72dfcd49c0ea772769d04d7e81ce0aceb2c1f7
Ricardo Coutinho <jrsscoutinho1@gmail.com>
2022-02-08 08:35:39 -0400

    RecyclerView added

---
+++ app/src/main/java/com/example/ocean_jornada_android_kotlin_recyclerview_multithread/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.ocean_jornada_android_kotlin_recyclerview_multithread
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
