9637f498bb6b7cf7ae9471e8100003d58a8fc4d7
Ricardo Coutinho <jrsscoutinho1@gmail.com>
2022-02-08 09:33:44 -0400

    Adapter created (WIP)

--- app/src/main/java/com/example/ocean_jornada_android_kotlin_recyclerview_multithread/PokemonsAdapter.kt
+++ app/src/main/java/com/example/ocean_jornada_android_kotlin_recyclerview_multithread/PokemonsAdapter.kt
@@ -1,9 +1,27 @@
 package com.example.ocean_jornada_android_kotlin_recyclerview_multithread
 
+import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 data class Pokemon(val nome: String, val imagemUrl: String)
 
-class PokemonsAdapter(val itens: List<Pokemon>) : RecyclerView.Adapter<PokemonsAdapter.ViewHoldeer> {
+class PokemonsAdapter(val itens: List<Pokemon>) : RecyclerView.Adapter<PokemonsAdapter.ViewHoldeer>() {
+    class ViewHoldeer(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHoldeer {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHoldeer, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 
 }
