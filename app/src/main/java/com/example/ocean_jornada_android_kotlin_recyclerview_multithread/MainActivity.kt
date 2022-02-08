2e03d3b94ccd604633ec7d7ae6d19f775143aaab
Ricardo Coutinho <jrsscoutinho1@gmail.com>
2022-02-08 10:41:01 -0400

    Correctly listing pokemons in recycleView

--- app/src/main/java/com/example/ocean_jornada_android_kotlin_recyclerview_multithread/MainActivity.kt
+++ app/src/main/java/com/example/ocean_jornada_android_kotlin_recyclerview_multithread/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.ocean_jornada_android_kotlin_recyclerview_multithread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rvPokemons = findViewById<RecyclerView>(R.id.rvPokemons)
+
+        // Layout Manager
+        rvPokemons.layoutManager = LinearLayoutManager(this)
+
+        // Adapter
+        val charmander = Pokemon(
+            "charmander",
+            "https://sm.ign.com/ign_br/screenshot/default/blob_bkmv.jpg")
+        val Pikachu = Pokemon(
+            "Pikachu",
+            "https://t2.tudocdn.net/301383?w=1920")
+        val listaPokemons = listOf(charmander, Pikachu, charmander)
+        rvPokemons.adapter = PokemonsAdapter(listaPokemons)
     }
 }
--- app/src/main/java/com/example/ocean_jornada_android_kotlin_recyclerview_multithread/PokemonsAdapter.kt
+++ app/src/main/java/com/example/ocean_jornada_android_kotlin_recyclerview_multithread/PokemonsAdapter.kt
@@ -1,26 +1,44 @@
 package com.example.ocean_jornada_android_kotlin_recyclerview_multithread
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 data class Pokemon(val nome: String, val imagemUrl: String)
 
-class PokemonsAdapter(val itens: List<Pokemon>) : RecyclerView.Adapter<PokemonsAdapter.ViewHoldeer>() {
-    class ViewHoldeer(itemView: View) : RecyclerView.ViewHolder(itemView) {
+class PokemonsAdapter(val itens: List<Pokemon>) :
+    RecyclerView.Adapter<PokemonsAdapter.ViewHolder>() {
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindView(item: Pokemon) = with(itemView) {
+            val tvNome = findViewById<TextView>(R.id.tvNome)
+            val ivPokemon = findViewById<ImageView>(R.id.ivPokemon)
+
+            tvNome.text = item.nome
+            Glide.with(this).load(item.imagemUrl).into(ivPokemon)
+        }
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHoldeer {
-        TODO("Not yet implemented")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+
+        val view = layoutInflater.inflate(R.layout.pokemon_item, parent, false)
+
+        return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHoldeer, position: Int) {
-        TODO("Not yet implemented")
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = itens[position]
+
+        holder.bindView(item)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return itens.count()
     }
 
 
